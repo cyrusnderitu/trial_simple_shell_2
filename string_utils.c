a29bc7f63834e2ff00ce3200eb1bf0e54e82f092
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-07-23 15:56:50 +0300

    Update string_utils.c for including _strlen

--- string_utils.c
+++ string_utils.c
@@ -1,5 +1,21 @@
 #include "shell.h"
 
+
+/**
+ *_strlen - calculates the length of a string
+ *@s: string parameter to calculate the length
+ *Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int countchar = 0;
+
+	while (s[countchar] != '\0')
+	{
+		countchar++;
+	}
+	return (countchar);
+}
 /**
  * _strcat - Concatenates two strings
  *  @dest: The string to be appended upon.
