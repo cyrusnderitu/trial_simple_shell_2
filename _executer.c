9665e6037c9f408e9ca4a74af4f5add25f36424b
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-07-23 15:45:49 +0300

    Update _executer.c for betty coding styles

--- _executer.c
+++ _executer.c
@@ -1,53 +1,37 @@
 #include "shell.h"
+
 /**
- * breakdown - creates child process and run execve
+ * _execute - creates child process and run execve
  * @ptr: pointer to the user input string for tokenizing
  */
-
 void _execute(char *ptr)
 {
 	pid_t child;
 	struct stat st;
-	char delim[] = " \n", *arr[1024], PATH[100] = "/bin/";
-	char *portion = strtok(ptr, delim);
+	char delim[] = " ", *arr[1024];
+	char *token = _strtok(ptr, delim);
 	int i = 0;
 
-	if (portion == NULL)
+	if (token == NULL)
 	{
-    free(ptr);
+    		free(ptr);
 		exit(0);
 	}
 	while (portion != NULL)
 	{
-		arr[i] = portion;
+		arr[i] = token;
 		i++;
-		portion = strtok(NULL, delim);
+		token = _strtok(NULL, delim);
 	}
-
-	switch (stat(arr[0], &st))
-		{
-			case 0:
-			{
-				child = fork();
-				break;
-			}
-			default:
-			{
-				if (_strcmp(arr[0], "exit") == 0)
-				{
-					kill(getpid(), SIGINT);
-				}
-				_strcat(PATH, arr[0]);
-				if (stat(PATH, &st) != 0)
-					perror("could not find file");
-				else
-				{
-					arr[0] = PATH;
-					child = fork();
-				}
-				break;
-			}
-		}
+	if (_strcmp(arr[0], "exit") == 0)
+	{
+		kill(getpid(), SIGINT);
+	}
+	else
+	{
+		child = fork();
+	}
+	break;	
 	if (child == 0 && (execve(arr[0], arr, NULL) == -1))
 		perror("Could not execute");
 }
