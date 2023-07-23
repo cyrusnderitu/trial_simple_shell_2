8b5449c5810b2b9a11444516d6c044b4c1248ba2
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-07-23 16:30:40 +0300

    Update shell.h for handling errors

--- shell.h
+++ shell.h
@@ -11,6 +11,7 @@
 char *_strcpy(char *dest, char *src);
 int  _strcmp(char *str1, char *str2);
 char *collect_args(void);
+void print_error(char *msg);
 char *_strcat(char *dest, char *src);
 int _strlen(char *s);
 char *_strtok(char *str, const char *delim);
