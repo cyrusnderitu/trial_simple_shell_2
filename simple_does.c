ba4d1330a759395694ccb8d3d4d7d95527cf6cc9
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-07-23 12:04:26 +0300

    Update simple_does.c

--- simple_does.c
+++ simple_does.c
@@ -59,33 +59,6 @@ s2++;
 }
 return (*s1 - *s2);
 }
-
-/**
- *collect_args - collects input from user
- *Return: pointer to array
- */
-char *collect_args(void)
-{
-	size_t len = 0;
-	char *ptr = NULL, *line = NULL;
-	ssize_t nread;
-
-	write(STDOUT_FILENO, "#cisfun$ ", 9);
-	while ((nread = getline(&line, &len, stdin)) != -1)
-	{
-		ptr = malloc(nread + 1);
-		_strcpy(ptr, line);
-		free(line);
-		break;
-	}
-	if (nread == -1)
-	{
-		write(1, "\n", 1);
-		exit(0);
-	}
-	return (ptr);
-}
-
 /**
  * breakdown - tokenizes the user input and parses it for evaluation
  * @ptr: pointer to the user input string for tokenizing
