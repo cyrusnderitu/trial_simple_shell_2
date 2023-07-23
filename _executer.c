cdb969cd068b81f8205665e8e2f5dde48fe23ebc
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-07-23 12:07:08 +0300

    Create _executer.c

---
+++ _executer.c
@@ -0,0 +1,53 @@
+#include "shell.h"
+/**
+ * breakdown - tokenizes the user input and parses it for evaluation
+ * @ptr: pointer to the user input string for tokenizing
+ */
+
+void breakdown(char *ptr)
+{
+	pid_t child;
+	struct stat st;
+	char delim[] = " \n", *arr[1024], PATH[100] = "/bin/";
+	char *portion = strtok(ptr, delim);
+	int i = 0;
+
+	if (portion == NULL)
+	{
+    free(ptr);
+		exit(0);
+	}
+	while (portion != NULL)
+	{
+		arr[i] = portion;
+		i++;
+		portion = strtok(NULL, delim);
+	}
+
+	switch (stat(arr[0], &st))
+		{
+			case 0:
+			{
+				child = fork();
+				break;
+			}
+			default:
+			{
+				if (_strcmp(arr[0], "exit") == 0)
+				{
+					kill(getpid(), SIGINT);
+				}
+				_strcat(PATH, arr[0]);
+				if (stat(PATH, &st) != 0)
+					perror("could not find file");
+				else
+				{
+					arr[0] = PATH;
+					child = fork();
+				}
+				break;
+			}
+		}
+	if (child == 0 && (execve(arr[0], arr, NULL) == -1))
+		perror("Could not execute");
+}
