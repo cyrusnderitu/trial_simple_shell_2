417e5f8e1427bcaeeb186b723dbcf3aa64c034b8
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-07-28 21:54:55 +0300

    Update _executer.c

--- _executer.c
+++ _executer.c
@@ -34,23 +34,10 @@ void finalizer(char *ptr)
         }
         default:
         {
-	if (arr[0] != NULL)
-	{
             if (_strcmp(arr[0], "exit") == 0)
             {
                 kill(getpid(), SIGINT);
             }
-	    else if (_strcmp(arr[0], "cd") == 0)
-            {
-                if (arr[1] != NULL)
-		{
-			if (chdir(arr[1]) == -1)
-                	{
-                    		perror("cd");
-                	}
-		}
-	    }
-	}
             _strcat(PATH, arr[0]);
             if (stat(PATH, &st) != 0)
                 perror("could not find file");
